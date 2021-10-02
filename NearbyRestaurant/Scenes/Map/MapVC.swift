4f766f0fa04fdec90e097e0c748c5d0495892e85
sherbeny96 <sherbeny2096@gmail.com>
2021-10-02 17:57:15 +0200

    Convert to git flow arct + Create app folders

---
+++ NearbyRestaurant/Scenes/Map/MapVC.swift
@@ -0,0 +1,23 @@
+//
+//  MapVC.swift
+//  NearbyRestaurant
+//
+//  Created by Mahmoud Sherbeny on 02/10/2021.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    init() {
+        super.init(nibName: "MapVC", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
